14e7c2cc77ead04068f6f46d0cf4641c1faee121
Harry Emeric <harryemeric@Harrys-MacBook-Pro-4.local>
2020-12-19 22:59:15 +0000

    add file structure for api calls

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
@@ -10,7 +11,55 @@ def create_app(test_config=None):
 
   return app
 
+
 APP = create_app()
+
+
+@APP.route('/')
+def hello_world():
+    return 'Hello, World from Flask!\n'
+
+
+@app.route('/actors', methods=['GET'])
+def get_actors():
+  pass
+
+
+@app.route('/movies', methods=['GET'])
+def get_movies():
+  pass
+
+
+@app.route('/actors/<int:actor_id>', methods=['POST'])
+def create_actor(actor_id):
+  body = request.get_json()
+
+
+@app.route('/movies/<int:movie_id>', methods=['POST'])
+def create_movie(movie_id):
+  body = request.get_json()
+
+
+@app.route('/actors/<int:actor_id>', methods=['DELETE'])
+def delete_actor(actor_id):
+  pass
+
+
+@app.route('/movies/<int:movie_id>', methods=['DELETE'])
+def delete_movie(movie_id):
+  pass
+
+
+@app.route('/actors/<int:actor_id>', methods=['PATCH'])
+def edit_actor(payload, actor_id):
+  pass
+
+
+@app.route('/movies/<int:movie_id>', methods=['PATCH'])
+def edit_movie(payload, movie_id):
+  pass
+
+
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
