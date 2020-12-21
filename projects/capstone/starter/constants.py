d3c4111f6187fe46c4f1e16e956274c72adcf846
Harry Emeric <harryemeric@Harrys-MacBook-Pro-4.local>
2020-12-21 22:22:18 +0000

    amend callback var for heroku

--- projects/capstone/starter/constants.py
+++ projects/capstone/starter/constants.py
@@ -2,7 +2,7 @@
 """
 #AUTH0_CLIENT_ID = 'AUTH0_CLIENT_ID'
 #AUTH0_CLIENT_SECRET = 'AUTH0_CLIENT_SECRET'
-#AUTH0_CALLBACK_URL = 'https://movie-casting-agency.herokuapp.com/callback'
+AUTH0_CALLBACK_URL = 'https://movie-casting-agency.herokuapp.com/callback'
 #AUTH0_DOMAIN = 'AUTH0_DOMAIN'
 #AUTH0_AUDIENCE = 'AUTH0_AUDIENCE'
 PROFILE_KEY = 'profile'
