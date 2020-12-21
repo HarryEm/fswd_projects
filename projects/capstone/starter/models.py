29e678359e447bc7ddc92b443295bbfce5702d1d
Harry Emeric <harryemeric@Harrys-MacBook-Pro-4.local>
2020-12-21 11:49:29 +0000

    database url now an enviornment variable

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -4,8 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 # import json
 
-database_name = "movie_casting"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+# database_name = "movie_casting"
+# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 migrate = Migrate()
